bd9bed31c5843138233dc60e3667a9051745eb2e
Ramudgar <ramudgary52@gmail.com>
2022-11-08 13:11:40 +0545

    signup form is made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {Navbar} from './components/navbar.js'
+import Navbar from './components/navbar.js'
+import {Signup} from './components/signup';
 
 function App() {
   return (
    <>
 <Navbar/>
+<Signup/>
 
 
    </>
---
+++ frontend/src/components/signup.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import '../css/signup.css'
+
+export class Signup extends Component{
+    render(){
+        return(
+            <>
+       <div className="signup-form">
+  <form action="/examples/actions/confirmation.php" method="post">
+    <h2>Create an Account</h2>
+    <p className="hint-text">
+      Sign up with your social media account or email address
+    </p>
+    <div className="social-btn text-center">
+      <a href="#" className="btn btn-primary btn-lg">
+        <i className="fa fa-linkedin" /> LinkedIn
+      </a>
+      
+      <a href="#" className="btn btn-danger btn-lg">
+        <i className="fa fa-google" /> Google
+      </a>
+    </div>
+    <div className="or-seperator">
+      <b>or</b>
+    </div>
+    <div className="form-group">
+      <input
+        type="text"
+        className="form-control input-lg"
+        name="username"
+        placeholder="Username"
+        required="required"
+      />
+    </div>
+    <div className="form-group">
+      <input
+        type="email"
+        className="form-control input-lg"
+        name="email"
+        placeholder="Email Address"
+        required="required"
+      />
+    </div>
+    <div className="form-group">
+      <input
+        type="password"
+        className="form-control input-lg"
+        name="password"
+        placeholder="Password"
+        required="required"
+      />
+    </div>
+    <div className="form-group">
+      <input
+        type="password"
+        className="form-control input-lg"
+        name="confirm_password"
+        placeholder="Confirm Password"
+        required="required"
+      />
+    </div>
+    <div className="form-group text-center p-1 ">
+      <button
+        type="submit"
+        className="btn btn-success btn-lg  btn-block signup-btn w-50 "
+      >
+        Sign Up
+      </button>
+    </div>
+  </form>
+  <div className="text-center">
+    Already have an account? <a href="#">Login here</a>
+  </div>
+</div>
+
+            </>
+        )
+            
+        
+    }
+}
