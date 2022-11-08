4ed18ac2bafe0ee16f53e17edcda50674898fee6
Milan Yadav <ymilan593@gmail.com>
2022-11-08 08:24:49 +0545

    refactor

---
+++ backend/apis/registerApi.js
@@ -0,0 +1,21 @@
+//for user registration
+
+const express = require("express");
+const router = express.Router();
+const Register = require("../models/registerModel");
+
+const userRegister = async (req, res) => {
+  try {
+    const registerUser = new Register({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const registered = await registerUser.save();
+    res.status(201).send(registered);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+exports.userRegister = userRegister;
--- backend/routes/register.js
+++
@@ -1,9 +0,0 @@
-//route for register
-
-const express = require('express');
-const router = express.Router();
-const { userRegister } = require('../middleware/register');
-
-router.post('/register', userRegister);
-
-module.exports = router;
