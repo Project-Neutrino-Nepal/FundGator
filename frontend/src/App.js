0473eafd431f59368bbfd41cbc51ff57382fcde4
Ramudgar <ramudgary52@gmail.com>
2022-11-08 14:46:38 +0545

    linking the routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar.js'
 import {Signup} from './components/signup';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-   <>
+   <BrowserRouter>
 <Navbar/>
-<Signup/>
+<Routes>
+  <Route path='/signup' element={<Signup/>}></Route>
+</Routes>
+{/* <Signup/> */}
 
-
-   </>
+   </BrowserRouter>
    
   );
 }
