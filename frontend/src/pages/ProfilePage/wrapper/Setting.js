451e22f5bb209367f4cf97e25d655077b4657bb2
Peisha <perisha.lama@gmail.com>
2022-11-16 20:15:39 +0545

    userinput component design was made

--- frontend/src/pages/ProfilePage/wrapper/Setting.js
+++ frontend/src/pages/ProfilePage/wrapper/Setting.js
@@ -5,8 +5,49 @@ const Wrapper = styled.div`
     display: none;
   }
 
+  .hover {
+    cursor: pointer;
+    padding: 2% 1%;
+    padding-block: 10px;
+    &:hover {
+      background-color: #f2f5f8;
+    }
+  }
+
+  .right-container {
+    margin-inline: 2%;
+
+    .userinput + .userinput{
+      margin-top: 5px;
+      
+    }
+    & > .account {
+      & > h1 {
+        font-size: 25px;
+      }
+      & > .passwordreset {
+        display: flex;
+        justify-content: space-between;
+
+        & > span + span {
+          color: #37cc9f;
+        }
+      }
+
+      & > .btn-delete {
+        width: 100%;
+        display: block;
+        text-align: end;
+        & > span {
+          color: red;
+        }
+      }
+    }
+  }
+
   @media screen and (min-width: 970px) {
     padding-top: 40px;
+    display: flex;
 
     .left-container {
       width: 20%;
@@ -15,6 +56,8 @@ const Wrapper = styled.div`
       font-size: 15px;
       line-height: 8px;
       color: var(--l-p-color);
+      position: sticky;
+      top: 0;
     }
 
     .right-container {
