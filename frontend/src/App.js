7874c711919aaa95e85dbaba09eed167bb0470b5
Peisha <perisha.lama@gmail.com>
2022-11-18 16:30:19 +0545

    explore page designe was done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/navbar.js'
 import Signup from './components/Signup';
 import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import { Signin } from './components/singin';
-import {LandingPage,ProfilePage,UserProfilePage,WelcomePage} from "./pages"
+import {LandingPage,ProfilePage,UserProfilePage,WelcomePage,ExplorePage} from "./pages"
 import Homepage from "./components/homepage"
 
 
@@ -20,6 +20,7 @@ function App() {
         <Route path="/profile" element={<UserProfilePage />}></Route>
         <Route path="/welcome" element={<WelcomePage />}></Route>
         <Route path="/homepage" element={<Homepage />}></Route>
+        <Route path="/explore" element={<ExplorePage />}></Route>
       </Routes>
     </BrowserRouter>
   );
