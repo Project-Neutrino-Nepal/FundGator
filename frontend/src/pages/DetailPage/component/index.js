9b7bf785256e73c3df1a7bfe911b5b62d56f08ed
Peisha <perisha.lama@gmail.com>
2022-11-23 18:54:08 +0545

    detail page was updated

--- frontend/src/pages/DetailPage/component/index.js
+++ frontend/src/pages/DetailPage/component/index.js
@@ -2,9 +2,15 @@ import InvestmentTerm from "./InvestmentTerm";
 import LeadInvestor from "./LeadInvestor";
 import Overview from "./Overview";
 import Detail from "./Detail"
+import WhatInvestorSay from "./WhatInvestorSay";
+import SingleComment from "./SingleComment";
+import AskAQuestion from "./AskAQuestion";
 export {
   LeadInvestor,
   InvestmentTerm,
   Overview,
-  Detail
+  Detail,
+  WhatInvestorSay,
+  SingleComment,
+  AskAQuestion,
 };
