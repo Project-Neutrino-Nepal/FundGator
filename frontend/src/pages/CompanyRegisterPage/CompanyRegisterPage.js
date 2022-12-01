476b2fb76e9df4c42f89db75e8fd25af248de9c9
Peisha <perisha.lama@gmail.com>
2022-12-01 20:36:11 +0545

    team tab was added in companyregister page

--- frontend/src/pages/CompanyRegisterPage/CompanyRegisterPage.js
+++ frontend/src/pages/CompanyRegisterPage/CompanyRegisterPage.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
-import { Basic, Visiblity, Story } from "./component";
+import { Basic, Visiblity, Story, Team } from "./component";
 import Wrapper from "./wrapper/CompanyRegisterPage";
 import tabs from "./utils/tab";
-import { toast, ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 import axios from "axios";
+import upload from "../../assets/image/uploadpic.svg";
 import { useParams } from "react-router-dom";
+
 const CompanyRegisterPage = () => {
+  const { id } = useParams();
+
   const formvalue = {
-    name: "",
+    companyname: id,
+
     city: "",
     facebook: "",
     linkedin: "",
@@ -16,77 +21,140 @@ const CompanyRegisterPage = () => {
     twitter: "",
     youtube: "",
     blog: "",
-    reason0: "",
-    reason1: "",
-    reason2: "",
-    reason3: "",
-    reason4: "",
-    reason5: "",
-    reason6: "",
-    reason7: "",
-    reason8: "",
-  };
-  const [activeindex, setActive] = useState(1);
-  const [values, setValue] = useState(formvalue);
-  const handleChange = (e) => {
-    setValue({ ...values, [e.target.name]: e.target.value });
+    reasons: [{ reason: "" }],
+    teams: [
+      {
+        id: 0,
+        image: "",
+        name: "name",
+        email: "email",
+        position: "CEO",
+        accomplished: "",
+        userfblink: "",
+        userlinkedinlink: "",
+        foundertype: "",
+        jobtype: "",
+        imagepreview: upload,
+      },
+    ],
+    imageupload: "",
+    videoupload: "",
+    imageuploadpreview: upload,
+    videouploadpreview: "",
+    linktype: "",
+    companyurl: "",
   };
   const data = {
-    city: values.city,
-    facebook: values.facebook,
-    linkedin: values.linkedin,
-    companylink: values.companylink,
-    twitter: values.twitter,
-    blog: values.blog,
-    reason0: values.reason0,
-    reason1: values.reason1,
-    reason2: values.reason2,
-    reason3: values.reason3,
-    reason4: values.reason4,
-    reason5: values.reason5,
-    reason6: values.reason6,
-    reason7: values.reason7,
-    reason8: values.reason8,
+    name: formvalue.name,
+    city: formvalue.city,
+    facebook: formvalue.facebook,
+    linkedin: formvalue.linkedin,
+    instagram: formvalue.instagram,
+    companylink: formvalue.companylink,
+    twitter: formvalue.twitter,
+    youtube: formvalue.youtube,
+    blog: formvalue.blog,
   };
-  // console.log(values);
-  const onsave = () => {
+  const createCompany = async (e) => {
+    e.preventDefault();
     const config = {
       headers: {
-        Authorization: localStorage.getItem("token"),
+        authorization: localStorage.getItem("token"),
       },
     };
-    if (activeindex <= 3) {
-      setActive((prev) => prev + 1);
-      if (activeindex == 1) {
-        try {
-          axios
-            .post(
-              "http://localhost:5000/company/reason/api/create-reason" +
-                `/${useParams.name}`,
-              values,
-              config
-            )
-            .then((res) => {
-              // if (res.data.success) {
-              //   toast.success(
-              //     res.data.message,
-                  
-              //   );
-              // }
-            });
-        } catch (error) {
-          toast.error(error.response.data.message);
-        }
-      }
+    try {
+      await axios
+        .post("http://localhost:5000/users/api/register", formvalue, config)
+        .then((res) => {
+          if (res.data.success) {
+            toast.success(
+              res.data.message,
+              setTimeout(function () {
+                window.location.assign("/signin");
+              }, 2000)
+            );
+          }
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
     }
   };
 
- 
-  console.log(data);
+  const [activeindex, setActive] = useState(1);
+  const [values, setValue] = useState(formvalue);
+  const reasonChange = (e, index) => {
+    var newreason = values.reasons;
+    newreason[index].reason = e.target.value;
+    setValue({ ...values, reasons: newreason });
+  };
+  const Addreason = () => {
+    var newReason = values.reasons;
+    if (newReason.length <= 7) {
+      newReason.push({ reason: "" });
+      setValue({ ...values, reasons: newReason });
+    }
+  };
 
+  const teamChange = (e, index, name) => {
+    var teams = values.teams;
+    if (e.target.files && e.target.files[0]) {
+      let file = e.target.files[0];
+      let blobURL = URL.createObjectURL(file);
+      teams[index] = {
+        ...teams[index],
+        [name]: e.target.files[0],
+        imagepreview: blobURL,
+      };
+
+      setValue({ ...values, teams: teams });
+    } else {
+      teams[index] = { ...teams[index], [name]: e.target.value };
+
+      setValue({ ...values, teams: teams });
+    }
+  };
+  const Addteam = (name, email) => {
+    var newTeam = values.teams;
+    newTeam.push({
+      id: 0,
+      image: "",
+      name: name,
+      email: email,
+      position: "CEO",
+      accomplished: "",
+      userfblink: "",
+      userlinkedinlink: "",
+      foundertype: "",
+      jobtype: "",
+      imagepreview: upload,
+    });
+    setValue({ ...values, teams: newTeam });
+  };
+
+  const handleChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      setValue({ ...values, [e.target.name]: e.target.files[0] });
+    } else {
+      setValue({ ...values, [e.target.name]: e.target.value });
+    }
+  };
+
+  const fileChange = (e, name) => {
+    let file = e.target.files[0];
+    let blobURL = URL.createObjectURL(file);
+    setValue({
+      ...values,
+      [e.target.name]: e.target.files[0],
+      [name]: blobURL,
+    });
+  };
+  const onsave = () => {
+    if (activeindex <= 3) {
+      setActive((prev) => prev + 1);
+    }
+  };
   return (
     <Wrapper>
-      <ToastContainer />
       <section className="tabs-container" style={{ marginTop: 80 }}>
         {tabs.map((item, index) => {
           return (
@@ -110,10 +178,31 @@ const CompanyRegisterPage = () => {
       </section>
       <section className="form-section">
         {activeindex === 1 ? (
-          <Basic values={values} handleChange={handleChange} />
+          <Basic
+            values={values}
+            handleChange={handleChange}
+            Addreason={Addreason}
+            reasonChange={reasonChange}
+          />
         ) : null}
-        {activeindex === 4 ? <Visiblity /> : null}
-        {activeindex === 3 ? <Story /> : null}
+        {activeindex === 4 ? (
+          <Visiblity handleChange={handleChange} values={values} />
+        ) : null}
+        {activeindex === 3 ? (
+          <Story
+            handleChange={fileChange}
+            imgpreview={values.imageuploadpreview}
+            vdpreview={values.videouploadpreview}
+          />
+        ) : null}
+        {activeindex === 2 ? (
+          <Team
+            values={values}
+            handleChange={handleChange}
+            Addteam={Addteam}
+            teamChange={teamChange}
+          />
+        ) : null}
       </section>
       <section className="save">
         <button onClick={onsave}>
