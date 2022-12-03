704c451bec7fe35c27f3001f4b2bd1bd547a4a09
Tulsi12 <katteltulsi9819@gmail.com>
2022-12-03 16:21:19 +0545

    TDD test

--- backend/tests/singup.test.js
+++ backend/tests/singup.test.js
@@ -1,32 +1,31 @@
-//using the path of User Model
-const User = require('../models/userModel');
-const mongoose =  require('mongoose');
+// const User = require('../models/userModel');
+// const mongoose =  require('mongoose');
 
 
-// const url = "mongodb+srv://Tulsi:qwerty12345@cluster0.bcsf13g.mongodb.net/?retryWrites=true&w=majority";
-const url  = "mongodb+srv://milan361:iZEK0AAW2n6p4ilc@cluster0.uanmf.mongodb.net/FundGator?retryWrites=true&w=majority";
+// // const url = "mongodb+srv://Tulsi:qwerty12345@cluster0.bcsf13g.mongodb.net/?retryWrites=true&w=majority";
+// const url  = "mongodb+srv://milan361:iZEK0AAW2n6p4ilc@cluster0.uanmf.mongodb.net/FundGator?retryWrites=true&w=majority";
 
 
-beforeAll(async () => {
- await mongoose.connect(url, {
- useNewUrlParser: true });
-});
-afterAll(async () => {
- await mongoose.connection.close();
-});
+// beforeAll(async () => {
+//  await mongoose.connect(url, {
+//  useNewUrlParser: true });
+// });
+// afterAll(async () => {
+//  await mongoose.connection.close();
+// });
 
-describe('User Schema test For inserting', () => {
-    // Testing for inserting user 
-     it('Add User testing inserting', () => {
-     const user = {
-     'name': 'sprint',
-     'email' : "sprint@gmail.com",
-     'password': 'agile11'
-     };
-     return User.create(user)
-     .then((result) => {
-     expect(result.name).toEqual('sprint');
-     });
-     });
-    })
+// describe('User Schema test For inserting', () => {
+//     // Testing for inserting user 
+//      it('Add User testing inserting', () => {
+//      const user = {
+//      'name': 'sprint',
+//      'email' : "sprint@gmail.com",
+//      'password': 'agile11'
+//      };
+//      return User.create(user)
+//      .then((result) => {
+//      expect(result.name).toEqual('sprint');
+//      });
+//      });
+//     })
 
