b99523aa3ff61738946c6845faa60032aa30b3a9
Milan Yadav <ymilan593@gmail.com>
2022-12-04 19:57:12 +0545

    BDD For Details Page

--- frontend-bdd/features/step_defination/auth.js
+++ frontend-bdd/features/step_defination/auth.js
@@ -2,105 +2,32 @@ const { expect } = require("chai");
 const { Given, When, Then, Before, After } = require("@cucumber/cucumber");
 const { Builder, By, Key, until, sleep } = require("selenium-webdriver");
 
+const driver = new Builder().forBrowser("chrome").build();
 const delay = 5000;
-Given("Test registration functionality", { timeout: 100000 }, async function () {
-  let driver = await new Builder().forBrowser("chrome").build();
-  await driver.get("http://localhost:3000/signup");
-  await driver.findElement(By.id("name")).sendKeys("test");
-  await driver.findElement(By.id("email")).sendKeys("test1@gmail.com");
-  await driver.findElement(By.id("password")).sendKeys("test1234");
-  await driver.findElement(By.id("confirmpassword")).sendKeys("test1234");
-  await driver.sleep(delay);
-  await driver.findElement(By.id("registerBtn")).click();
 
-  await driver.wait(until.elementLocated(By.id("registerForm")), 100000);
-  expect(await driver.wait(until.elementLocated(By.id("registerForm"))));
-  await driver.quit();
+Given("I am on the login page", async function () {
+  await this.driver.get("http://localhost:3000/signin");
+  await this.driver.wait(until.elementLocated(By.id("loginForm")), delay);
 });
 
-Given("Test login functionality", { timeout: 100000 }, async function () {
-  let driver = await new Builder().forBrowser("chrome").build();
-  await driver.get("http://localhost:3000/signin");
-  await driver.findElement(By.id("email")).sendKeys("test@gmail.com");
-  await driver.findElement(By.id("password")).sendKeys("test@1234");
-  await driver.sleep(delay);
-  await driver.findElement(By.id("loginBtn")).click();
-
-  await driver.wait(until.elementLocated(By.id("loginForm")), 100000);
-  expect(await driver.wait(until.elementLocated(By.id("loginForm"))));
-  await driver.quit();
-});
-
-
-const { expect } = require("chai");
-const { Given, When, Then } = require("@cucumber/cucumber");
-const webdriver = require("selenium-webdriver");
-const { By, until } = require("selenium-webdriver");
-let driver = new webdriver.Builder().forBrowser("chrome").build();
-Given(
-  "I visit Crowdly Registration Page",
-  { timeout: 1000 * 1000 },
-  async () => {
-    await driver.get("http://localhost:3000/login");
+When(
+  "I enter {string} as username and I enter {string1} as password",
+  async function (username, password) {
+    username = await this.driver.findElement(By.id("email"));
+    password = await this.driver.findElement(By.id("password"));
+    await this.driver.findElement(By.id("email")).sendKeys(username);
+    await this.driver.findElement(By.id("password")).sendKeys(password);
   }
 );
-When("I enter my fullname", async () => {
-  await driver
-    .findElement(By.xpath("//input[@placeholder='Enter Fullname']"))
-    .sendKeys("Test User");
+
+When("I click on the login button", async function () {
+  await this.driver.findElement(By.id("login")).click();
 });
-When("I enter my username", async () => {
-  await driver
-    .findElement(By.xpath("//input[@placeholder='Enter Username']"))
-    .sendKeys("TestUser122323");
+
+Then("I should be redirected to the home page", async function () {
+  await this.driver.wait(until.elementLocated(By.id("profileUpdate")), delay);
+  const url = await this.driver.getCurrentUrl();
+  expect(url).to.equal("http://localhost:3000/Welcome");
 });
-When("I enter my email", async () => {
-  await driver
-    .findElement(By.name("email"))
-    .sendKeys("testuser122332@gmail.com");
-});
-When("I select applicant", async () => {
-  let element = driver.wait(
-    until.elementLocated(By.xpath("//div[@class='singleselectinput']"))
-  );
-  element.click();
-  let candidate = driver.wait(
-    until.elementLocated(By.xpath("//li[normalize-space()='individual']"))
-  );
-  candidate.click();
-});
-When("I enter my password", async () => {
-  await driver.findElement(By.name("password")).sendKeys("password");
-});
-When("I press submit", async () => {
-  let submit = driver.wait(
-    until.elementLocated(By.xpath("//button[normalize-space()='Submit']"))
-  );
-  submit.click();
-});
-When("I press login now", async () => {
-  let loginNow = driver.wait(
-    until.elementLocated(By.xpath("//p[@class='toggle-account']"))
-  );
-  loginNow.click();
-});
-When("I press login", async () => {
-  let loginNow = driver.wait(
-    until.elementLocated(By.xpath("//button[normalize-space()='Submit']"))
-  );
-  loginNow.click();
-});
-Then("I should be loggedin", async () => {
-  driver
-    .wait(until.elementLocated(By.id("successFail")))
-    .getText()
-    .then((text) => {
-      if (text === "User Created!: Redirection...") {
-        console.log(text);
-        return true;
-      } else {
-        console.log("Fail");
-        return false;
-      }
-    });
-});
+
+
