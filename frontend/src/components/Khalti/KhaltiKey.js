9d1d7031ae74ad8789e79c7040a28bb00d733074
Tulsi12 <katteltulsi9819@gmail.com>
2022-12-12 22:54:10 +0545

    Refactor

--- frontend/src/components/Khalti/KhaltiKey.js
+++ frontend/src/components/Khalti/KhaltiKey.js
@@ -1,6 +1,11 @@
-let myKey = {
-  publicTestKey: "",
-  SecretKey: "",
-};
+let myKey ={
+
+    publicTestKey:'test_public_key_88b0c62cb4334d86a594d39e2b2cdce9',
+
+    SecretKey:'test_secret_key_7c2cccee06744ff79ba99f3acd2efd4a'
+
+}
+
+
 
 export default myKey;
